0a35f47b70e10bdc38ee7dada7fa23f48ac69d60
Vick Greenfields <akintoluvic@users.noreply.github.com>
2019-09-21 17:02:18 +0100

    nodemon working

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) =>{
-    res.end('This is my server');
+    res.end('This is my Node with -mon server');
 })
 
 server.listen(process.env.PORT || 3000);
