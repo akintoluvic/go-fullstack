f8324d488a02397df918073d15064cc85f64fc16
Vick Greenfields <akintoluvic@users.noreply.github.com>
2019-09-21 17:15:44 +0100

    express working

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 const http = require('http');
+const app = require('./app');
 
-const server = http.createServer((req, res) =>{
-    res.end('This is my Node with -mon server');
-})
+app.set('port', process.env.PORT || 3000);
+
+const server = http.createServer(app);
 
 server.listen(process.env.PORT || 3000);
