51b7a4271d43c6043d2b00eea36ad2535b313e40
Vick Greenfields <akintoluvic@users.noreply.github.com>
2019-09-23 17:33:36 +0100

    get data working

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+const Thing = require('./models/thing');
+
 const app = express();
 
 mongoose.connect('mongodb+srv://green:916HyyV9rcDjQexN@cluster0-79dqk.mongodb.net/test?retryWrites=true&w=majority')
@@ -23,32 +25,38 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 
 app.post('/api/stuff', (req, res, next) => {
-    console.log(req.body);
-    res.status(201).json({
-        message: 'Thing created successfully'
+    const thing = new Thing({
+      title: req.body.title,
+      description: req.body.description,
+      imageUrl: req.body.imageUrl,
+      price: req.body.price,
+      userId: req.body.userId
     });
+    thing.save().then(
+      () => {
+        res.status(201).json({
+          message: 'Thing saved successfully!'
+        })
+      }
+    ).catch(
+      (error) => {
+        res.status(400).json({
+          error: error
+        })
+      }
+    )
 });
 
 app.use('/api/stuff', (req, res, next) => {
-    const stuff = [
-        {
-            _id: 'ghezhoi',
-            title: 'My first thing',
-            description: 'All of the info describing my first thing',
-            imageUrl: '',
-            price: '4900',
-            userId: 'ghezhoi',
-          },
-          {
-            _id: 'ghezhoiz',
-            title: 'My Second thing',
-            description: 'All of the info describing my second thing',
-            imageUrl: '',
-            price: '5900',
-            userId: 'ghezhoiz',
-          }
-    ];
-    res.status(200).json(stuff);
+    Thing.find().then(
+      (things) => {
+        res.status(200).json(things);
+      }
+    ).catch(
+      (error) => {
+        error: error
+      }
+    )
 });
 
 
