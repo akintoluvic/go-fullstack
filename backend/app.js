058bf6c07fe908826eeee95a7d6dd6f75c72332f
Vick Greenfields <akintoluvic@users.noreply.github.com>
2019-09-28 15:12:43 +0100

    fix

--- backend/app.js
+++ backend/app.js
@@ -25,38 +25,56 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 
 app.post('/api/stuff', (req, res, next) => {
-    const thing = new Thing({
-      title: req.body.title,
-      description: req.body.description,
-      imageUrl: req.body.imageUrl,
-      price: req.body.price,
-      userId: req.body.userId
-    });
-    thing.save().then(
-      () => {
-        res.status(201).json({
-          message: 'Thing saved successfully!'
-        })
-      }
-    ).catch(
-      (error) => {
-        res.status(400).json({
-          error: error
-        })
-      }
-    )
+  const thing = new Thing({
+    title: req.body.title,
+    description: req.body.description,
+    imageUrl: req.body.imageUrl,
+    price: req.body.price,
+    userId: req.body.userId
+  });
+  thing.save().then(
+    () => {
+      res.status(201).json({
+        message: 'Post saved successfully!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+});
+
+app.get('/api/stuff/:id', (req, res, next) => {
+  Thing.findOne({
+    _id: req.params.id
+  }).then(
+    (thing) => {
+      res.status(200).json(thing);
+    }
+  ).catch(
+    (error) => {
+      res.status(404).json({
+        error: error
+      });
+    }
+  );
 });
 
 app.use('/api/stuff', (req, res, next) => {
-    Thing.find().then(
-      (things) => {
-        res.status(200).json(things);
-      }
-    ).catch(
-      (error) => {
+  Thing.find().then(
+    (things) => {
+      res.status(200).json(things);
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
         error: error
-      }
-    )
+      });
+    }
+  );
 });
 
 
